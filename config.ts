0751eb08f1501aa7a3256ec840908e3358ac1cd0
Senee Lou <351774194@qq.com>
2025-12-20 01:33:05 +0800

    feat: Update app version to 1.0.4
    Update both `manifest.json` and `config.ts` to reflect the latest application version. This is a routine version bump.

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 
 export const APP_CONFIG = {
-  VERSION: '1.0.1',
+  VERSION: '1.0.4',
   GITHUB_URL: 'https://github.com/lustan/CaptureApi',
   FEEDBACK_URL: 'https://github.com/lustan/CaptureApi/issues',
 };
